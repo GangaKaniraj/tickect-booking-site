cbed1ff97ab306925cdbaf258b6f4084705f44ef
Ganga K <nkganga90@gmail.com>
2021-06-17 18:27:58 +0530

    modified

--- main.js
+++ main.js
@@ -3,3 +3,6 @@
 function changebackgroundColor() {
     document.getElementById("signin").style.backgroundColor = "yellow";
 }
+
+let h = document.getElementsByTagName ('h1') [0]
+h.style.color = 'white'
